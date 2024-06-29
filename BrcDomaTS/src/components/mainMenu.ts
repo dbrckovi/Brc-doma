373a460bc64195c482fbce8f2e03fb9c4065d75b
Davor Brcković <dbrckovi@gmail.com>
2024-06-29 22:07:27 +0200

    sitnice

--- BrcDomaTS/src/components/mainMenu.ts
+++ BrcDomaTS/src/components/mainMenu.ts
@@ -1,14 +1,16 @@
+import { PAGE_ID_CAMERAS, PAGE_ID_DEVICES, PAGE_ID_LISTS, PAGE_ID_MUSIC_CONTROL, PAGE_ID_TEXTS } from "global";
+
 export class MainMenu {
     constructor(private container: HTMLElement, private onNavigate: (module: string) => void) {}
 
     render() {
         const menuItems = [
-            { name: 'List', id: 'lists', disabled: false },
-            { name: 'Texts', id: 'texts', disabled: false },
-            { name: 'Devices', id: 'devices', disabled: true},
-            { name: 'Light Control', id: 'lightControl', disabled: true },
-            { name: 'Music Control', id: 'musicControl', disabled: true },
-            { name: 'Cameras', id: 'cameras', disabled: true },
+            { name: 'List', id: PAGE_ID_LISTS, disabled: false },
+            { name: 'Texts', id: PAGE_ID_TEXTS, disabled: false },
+            { name: 'Devices', id: PAGE_ID_DEVICES, disabled: true},
+            { name: 'Light Control', id: PAGE_ID_LISTS, disabled: true },
+            { name: 'Music Control', id: PAGE_ID_MUSIC_CONTROL, disabled: true },
+            { name: 'Cameras', id: PAGE_ID_CAMERAS, disabled: true },
         ];
 
         menuItems.forEach(item => {
--- BrcDomaTS/src/global.ts
+++ BrcDomaTS/src/global.ts
@@ -3,3 +3,7 @@ export const mode = process.env.MODE as 'development' | 'production'
 
 export const PAGE_ID_LISTS = 'lists';
 export const PAGE_ID_TEXTS = 'texts';
+export const PAGE_ID_DEVICES = 'devices';
+export const PAGE_ID_LIGHT_CONTROL = 'lightControl';
+export const PAGE_ID_MUSIC_CONTROL = 'musicControl';
+export const PAGE_ID_CAMERAS = 'cameras';
